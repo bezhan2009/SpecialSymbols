5cbd6f3d8756d4dcd780499ebf0d6d0b17c34708
Bejan <157696306+bezhan2009@users.noreply.github.com>
2024-07-07 17:57:38 +0300

    Delete simple_interest directory

--- simple_interest/simple_interest.lisp
+++
@@ -1,7 +0,0 @@
-(defun calculate-simple-interest (principal rate time)
-  (/ (* principal rate time) 100))
-
-(let ((principal 1000)
-      (rate 5)
-      (time 3))
-  (format t "The simple interest is: ~a~%" (calculate-simple-interest principal rate time)))
